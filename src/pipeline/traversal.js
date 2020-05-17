1c23bc8bbf05ace89eecdc722cefd4aeea9f7058
Mark Rickerby <me@maetl.net>
2020-05-18 01:53:17 +1200

    Ensure traversal API supports all step capabilities

--- src/pipeline/traversal.js
+++ src/pipeline/traversal.js
@@ -68,7 +68,7 @@ class Traversal {
   }
 
   where(predicate) {
-    this.chain.connect(WhereStep, predicate);
+    this.chain.connect(WhereStep, predicate, this.graph);
   }
 
   filter(predicate) {
