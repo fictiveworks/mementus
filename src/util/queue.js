f92fb877735b8e360388269a14b6077ce2ff8b77
Mark Rickerby <me@maetl.net>
2022-02-15 22:54:22 +1300

    Tidy up fifo queue and add unit test

--- src/util/queue.js
+++ src/util/queue.js
@@ -1,37 +1,38 @@
 // Simple queue which uses an array as storage.
 //
-// Based on the original idea by Stephen Morley.
-// See: http://code.stephenmorley.org/javascript/queues/
+// Based on the original idea by Kate Rose Morley.
 //
 // @maetl / 2017
-function Queue() {
-  this._entries = [];
-  this._offset = 0;
-}
-
-Queue.prototype.add = function(entry) {
-  this._entries.push(entry);
-}
-
-Queue.prototype.isEmpty = function(){
-  return (this._entries.length - this._offset) == 0;
-}
-
-Queue.prototype.removeFirst = function() {
-  if (this._entries.length == 0) return;
-
-  var entry = this._entries[this._offset];
-
-  this._offset++;
-
-  // If the space left at the front takes up half of the array then
-  // slice it off and reset the array offset
-  if (this._offset * 2 > this._entries.length) {
-    this._entries = this._entries.slice(this._offset);
+class Queue {
+  constructor() {
+    this._entries = [];
     this._offset = 0;
   }
 
-  return entry;
+  enqueue(entry) {
+    this._entries.push(entry);
+  }
+
+  dequeue() {
+    if (this._entries.length == 0) return;
+
+    const entry = this._entries[this._offset];
+
+    this._offset++;
+
+    // If the space left at the front takes up half of the array then
+    // slice it off and reset the array offset
+    if (this._offset * 2 > this._entries.length) {
+      this._entries = this._entries.slice(this._offset);
+      this._offset = 0;
+    }
+
+    return entry;
+  }
+
+  isEmpty() {
+    return (this._entries.length - this._offset) == 0;
+  }
 }
 
 export default Queue;
---
+++ test/util/queue.test.js
@@ -0,0 +1,18 @@
+import test from "ava";
+import Queue from "../../src/util/queue";
+
+test("queue starts empty", t => {
+  const queue = new Queue();
+  t.is(queue.isEmpty(), true);
+});
+
+test("first in, first out", t => {
+  const queue = new Queue();
+  queue.enqueue("first");
+  queue.enqueue("second");
+  queue.enqueue("third");
+
+  t.is(queue.dequeue(), "first");
+  t.is(queue.dequeue(), "second");
+  t.is(queue.dequeue(), "third");
+});
