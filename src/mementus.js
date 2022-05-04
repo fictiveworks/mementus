21e5604d5a11b81c4bef28cc3adc4e21407846fc
Mark Rickerby <me@maetl.net>
2022-05-04 15:33:49 +1200

    Replace createGraph function which was not present in the source

--- src/mementus.js
+++ src/mementus.js
@@ -1,8 +1,12 @@
-import Graph, { createGraph } from "./graph";
+import Graph from "./graph";
 import Node from "./node";
 import Edge from "./edge";
 import Queue from "./util/queue";
 import PriorityQueue from "./util/priority-queue";
+
+function createGraph(initializer) {
+  return new Graph(initializer);
+}
 
 export {
   createGraph,
